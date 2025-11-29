387782dbbba3d9e72464af6181c16d552b9460f9
shysssthanhtri <shyssthanhtri@gmail.com>
2025-11-29 13:59:33 +0700

    Correction

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/context/Theme";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -30,8 +31,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ const Home = () => {
   return (
     <>
       <h1 className="h1-bold">Welcome to the world of Next.js</h1>
-      <h1 className="h1-bold font-space-grotesk">Welcome to the world of Next.js</h1>
     </>
   );
 };
