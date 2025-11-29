ccb94f1f0b7e0ee0b1e93128f41277eb5957fda9
shysssthanhtri <shyssthanhtri@gmail.com>
2025-11-29 11:39:31 +0700

    setup fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
-
 const Home = () => {
-    return (<h1 className="text-3xl">Welcome to the world of Next.js</h1>)
+    return (
+        <>
+            <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+            <h1 className="h1-bold font-space-grotesk">Welcome to the world of Next.js</h1>
+        </>
+    )
 }
 
 export default Home
