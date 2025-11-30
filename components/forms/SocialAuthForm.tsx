f209dd29db6304d101d81360460c16430cb04660
shysssthanhtri <shyssthanhtri@gmail.com>
2025-11-30 22:28:00 +0700

    SignIn with GitHub

--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,12 +1,42 @@
+"use client";
+
 import React from "react";
 import { SocialButton } from "@/components/forms/SocialButton";
+import { toast } from "sonner";
+import { signIn } from "next-auth/react";
+import { ROUTES } from "@/constants/routes";
 
 export const SocialAuthForm = () => {
+  const handleSignIn = async (prodiver: "github" | "google") => {
+    try {
+      await signIn(prodiver, {
+        redirectTo: ROUTES.HOME,
+        // redirect: false,
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error("Sign-in failed.", {
+        description: error instanceof Error ? error.message : undefined,
+      });
+    }
+  };
+
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-      <SocialButton iconSrc="/icons/github.svg" iconAlt="GitHub Logo" label="Log in with GitHub" invertIcon />
+      <SocialButton
+        iconSrc="/icons/github.svg"
+        iconAlt="GitHub Logo"
+        label="Log in with GitHub"
+        invertIcon
+        onClick={() => handleSignIn("github")}
+      />
 
-      <SocialButton iconSrc="/icons/google.svg" iconAlt="Google Logo" label="Log in with Google" />
+      <SocialButton
+        iconSrc="/icons/google.svg"
+        iconAlt="Google Logo"
+        label="Log in with Google"
+        onClick={() => handleSignIn("google")}
+      />
     </div>
   );
 };
