eace90e52a64a4625a8c21d9d36fc0497759caad
shysssthanhtri <shyssthanhtri@gmail.com>
2025-12-01 18:12:18 +0700

    Implement home page layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ROUTES } from "@/constants/routes";
+
 const Home = () => {
   return (
     <>
-      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button className="primary-gradient !text-light-900 min-h-[46px] px-4 py-3">
+          <Link href={ROUTES.ASK_A_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 2</p>
+        <p>Question Card 3</p>
+        <p>Question Card 4</p>
+      </div>
     </>
   );
 };
